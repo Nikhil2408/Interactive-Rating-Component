0d08627c2e34e54b54827499475beb8fa2899445
Nikhil Bansal <nikbansal1998@gmail.com>
2022-03-30 09:23:28 +0530

    fix: fixed styling and rating repeating multiple times

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const ratings = document.querySelectorAll("a.number");
+const btn = document.getElementById("btn");
 
 let selectedRating = 0;
+
 for(let rating of ratings){
     rating.addEventListener("click", function(){
         for(let rating of ratings)
@@ -12,13 +14,9 @@ for(let rating of ratings){
     });   
 }
 
-
-const btn = document.querySelector(".btn");
 btn.addEventListener("click", function(){
-    const displaySelectedRating = document.createElement("p");
+    const displaySelectedRating = document.querySelector("#page-2 > .rating-component > #displayRating");
     displaySelectedRating.innerText = `You selected ${selectedRating} out of 5`;
     displaySelectedRating.classList.add("displayRating");
-    const tqHeading = document.querySelector("#tq-heading");
-    tqHeading.insertAdjacentElement("beforebegin", displaySelectedRating);
 });
 
